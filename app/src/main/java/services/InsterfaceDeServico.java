00f5a2dc160017a14432a890e7b80b9ac4db3990
Aluno <aluno@udicentro.local>
2018-10-29 21:14:32 -0300

    produto

--- app/src/main/java/services/InsterfaceDeServico.java
+++ app/src/main/java/services/InsterfaceDeServico.java
@@ -12,19 +12,8 @@ import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
 
-public class InsterfaceDeServico {
-    @GET("/categorias/{id}")
-    Call<Categoria> getCategoria(@Path("id") int id);
+public interface InsterfaceDeServico {
 
-    @GET("/categorias")
-    Call<List<Categoria>> getCategorias();
-
-    @DELETE("/produtos/{id}")
-    Call<ResponseBody> removeProduto(@Path("id") int id);
-
-    @POST("/categorias")
-    Call<ResponseBody> salvar(@Body Categoria categoria);
-
-    @POST("/produtos")
-    Call<ResponseBody> salvarProduto(@Body Produto produto);
+    @GET("/precos")
+    Call<List<Produto>> getListCall(@Path("id") int id);
 }
